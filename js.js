eb603b2da4caa8e4d7e4f56c493c618cf469ffa0
DevBDarius <d.burinskis@gmail.com>
2021-12-01 20:10:44 +0000

    init commit

---
+++ js.js
@@ -0,0 +1,8 @@
+// API key: 8af22d65
+// API request format: https://www.omdbapi.com/?apikey=8af22d65&s=[moviename]
+// Parameters:
+// s - input title, output 10 first search results
+// t - input title, output movie details
+// page - input N, output Nth page of search result
+
+console.log('hi! JS works');
